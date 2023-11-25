9402be4e164ae3c1ae4a911765a2b29816530973
Chloe Caddell <chloecaddell3@gmail.com>
2023-11-24 21:09:15 -0600

    Navigation Part 3

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import Main from "./screens/MainComponent";
+import { NavigationContainer } from "@react-navigation/native";
 
 function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
   );
 };
 
